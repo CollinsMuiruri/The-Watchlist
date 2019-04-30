41771111dfc0f0811256d9e712549fe6ca7ffa59
Collins Muiruri <wanyekicollins@gmail.com>
2019-04-30 11:25:18 +0300

    Implement control blocks

--- app/views.py
+++ app/views.py
@@ -7,8 +7,9 @@ def index():
     '''
     View  root page function that returns the index page and its data
     '''
+    title = "Home - Welcome To The Best Movie Review Website Online"
     message = 'Hello from the other side!!'
-    return render_template('index.html', message = message)
+    return render_template('index.html', message = message, title = title)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
