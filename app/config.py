2053fca2ed440fd89e3ac7e04a6c7c788ea936e8
Collins Muiruri <wanyekicollins@gmail.com>
2019-04-30 12:39:50 +0300

    Create movie class and input API key

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    MOVIE_API_BASE_URL = https://api.themoviedb.org/3/movie/550?api_key=52c40cdeb693f66cf9801ff9af468154
 class ProdConfig(Config):
     '''
     Production configuration child class
