0ac16b773adaf764e2a7112619f2c657aa975710
Collins Muiruri <wanyekicollins@gmail.com>
2019-05-03 15:47:13 +0300

    Add error page and search function

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for
 from app import app
-from .request import get_movies,get_movie
+from .request import get_movies,get_movie,search_movie
 
 #Views
 @app.route('/')
@@ -15,6 +15,13 @@ def index():
     now_showing_movies = get_movies('now_playing')
     title = "Home - Welcome To The Best Movie Review Website Online"
     message = 'Hello from the other side!!'
+
+    search_movie = request.args.get('movie_query')
+
+    if search_movie:
+        return redirect(url_for('search', movie_name = search_movie))
+    else:
+        return render_template('index.html', message = message, title = title, popular = popular_movies, upcoming = upcoming_movies, now_showing = now_showing_movies)
     return render_template('index.html', message = message, title = title, popular = popular_movies, upcoming = upcoming_movies, now_showing = now_showing_movies)
 
 @app.route('/movie/<int:id>')
@@ -25,3 +32,14 @@ def movie(id):
     movie = get_movie(id)
     title = f'{movie.title}'
     return render_template('movie.html',title = title, movie = movie)
+
+@app.route('/search/<movie_name>')
+def search(movie_name):
+    '''
+    View function to display search results
+    '''
+    movie_name_list = movie_name.split(" ")
+    movie_name_format = "+".join(movie_name_list)
+    searched_movies = search_movie(movie_name_format)
+    title = f'search results for {movie_name}'
+    return render_template('search.html', movies = searched_movies)
